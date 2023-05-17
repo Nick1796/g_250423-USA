6d2e0e1e42a582700a6388ffcca3da7641bf6fd4
Nick1796 <nikitaz1796@yahoo.com>
2023-05-16 21:37:36 -0400

    test

---
+++ src/main/java/Lesson5/task1/LogicalTest.java
@@ -0,0 +1,28 @@
+package Lesson5.task1;
+
+public class LogicalTest {
+
+    public static void main(String[] args) {
+
+        int a = 15;
+        int b = 10;
+        int c = 20;
+        int d = 20;
+
+
+        boolean result1 = a > b;
+
+        boolean result2 = c == d;
+
+        boolean result = a > b && c == d;
+        boolean totalResult = result1 && result2;
+        System.out.println("result: " + result);
+        System.out.println(result1 + " " + result2 + " " + totalResult);
+
+        System.out.println(b <= a && c < a);
+        System.out.println(c <= b || a > d || c<= a);
+
+
+
+    }
+}
---
+++ src/main/java/Lesson5/task2/IfTest.java
@@ -0,0 +1,19 @@
+package Lesson5.task2;
+
+public class IfTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Начало");
+
+        int a = 10;
+        int b = 15;
+        boolean result = a > b;
+
+        if (result) {
+            System.out.println(" A больше, чем B");
+
+        }
+        System.out.println("Конец");
+    }
+}

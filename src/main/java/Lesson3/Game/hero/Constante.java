a9c65e9e579b38cd6a60f1e2f4909b12ec6bbe4b
Nick1796 <nikitaz1796@yahoo.com>
2023-05-17 17:02:50 -0400

    test

---
+++ src/main/java/Lesson3/Game/hero/Constante.java
@@ -0,0 +1,12 @@
+package Lesson3.Game.hero;
+
+public class Constante {
+    public static final String elf_power = "power";
+    public static final String elf_charisma = "charisma";
+
+    public static final String elf_magic = "magic";
+
+
+
+
+}
